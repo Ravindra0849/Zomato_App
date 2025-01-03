98b4a78da575c8c7cac0286d726b0f8b794044ad
Ravindra Reddy <148531100+Ravindra0849@users.noreply.github.com>
2025-01-03 22:26:29 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://3.111.23.2:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMSS',
+	    'sonar.login': 'squ_8b5bf1e9057b32ea49a542206eadec00089c43cf', // This is Sonar Token
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
