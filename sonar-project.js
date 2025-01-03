28b76ed6e4456c0855324aa9996b06e8b7be91df
Ravindra Reddy <148531100+Ravindra0849@users.noreply.github.com>
2025-01-03 22:28:51 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -15,3 +15,20 @@ sonarqubeScanner({
           //'sonar.inclusions' : 'src/**'
        },
 }, () => {});
+
+
+// npm install
+
+//node app.js
+
+//(OR)
+
+//npm start
+
+//To Execute the SonarQube Repor, execute the below command.
+
+//npm run sonar
+
+//(OR)
+
+//node sonar-project.js
